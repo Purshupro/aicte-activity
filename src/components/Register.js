a7741471ca214cd5e199cf72e6374cfc5df4ddba
purshotham <purshothamam@gmail.com>
2021-11-27 10:29:52 +0530

    Registration flow completed

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,33 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Registration = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
 
-    // const registerUser = () => {
-    //     console.log('user');
-    // }
+    const registerUser = (e) => {
+        e.preventDefault();
+        alert(`The name you entered was: ${userName}`);
+        fetch('http://localhost:5000/api/v1/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                user_id: userName,
+                password: password,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
 
     return (
         <div className="login-register">
             <h2>Register for a new account</h2>
-            <form action="" method="post">
+            <form onSubmit={registerUser} method="post">
                 <div className="formInput">
                     <label htmlFor="userName">Username</label>
                     <div>
-                        <input type="email" name="userName" id="userName" />
+                        <input type="email" name="userName" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} />
                     </div>
                 </div>
                 <div className="formInput">
                     <label htmlFor="password">Password</label>
                     <div>
-                        <input type="password" name="password" id="password" />
+                        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
                 </div>
-                <div className="formInput">
+                {/* <div className="formInput">
                     <label htmlFor="confirmPassword">Confirm Password</label>
                     <div>
                         <input type="text" name="confirmPassword" id="confirmPassword" />
                     </div>
-                </div>
+                </div> */}
                 <div className="buttonContainer">
                     <button type="submit" className="registerBtn">Register</button>
                     <span className="loginBtnContainer">
